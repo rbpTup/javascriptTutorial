72a59433e0939f3f576eb68433e1ea879f58444c
enigma01Z313 <f.ahmadyf94@gmail.com>
2021-03-07 18:52:07 +0330

    session1

---
+++ js/main.js
@@ -0,0 +1,61 @@
+// data types
+/*
+let firstName;
+console.log(typeof firstName);
+
+firstName = 12;
+console.log(typeof firstName);
+
+firstName = true;
+// firstName = false;
+console.log(typeof firstName);
+
+firstName = 'farzin';
+console.log(typeof firstName);
+
+firstName = '12';
+console.log(typeof firstName);
+
+// not for now 
+firstName = [];
+console.log(typeof firstName);
+
+firstName = {};
+console.log(typeof firstName);
+// end 
+*/
+
+
+
+
+
+
+// let firstName = `farzin`;
+// let age = 12;
+// console.log('My name is:'+age);
+// console.log(typeof ('My name is:'+age));
+// console.log(`My name is: ${firstName}`)
+
+
+
+
+
+
+// var firstName;
+// const lastName = 'farzin';
+// let age = 12;
+
+
+
+
+
+function printLastName() {
+  console.log('ahmadi');
+}
+
+function printFirstName() {
+  console.log('farzin');
+}
+
+
+// printLastName();
