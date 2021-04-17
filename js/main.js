85501bf513466427ade143a15722e8a2eaa79cce
enigma01Z313 <f.ahmadyf94@gmail.com>
2021-04-17 22:09:50 +0430

    session4

--- js/main.js
+++ js/main.js
@@ -1,88 +1,157 @@
-// console.log(this);
-/* Functions ***************** */
-// let a, b, sumNum, c;
-
-// function sum(g, h){
-//   return g + h;
+/* Events ****************** */
+// function printFirstName() {
+//   console.log("Farzin");
 // }
 
-// function multiple(b, n){
-//   return b * n;
+// function printVal(value){
+//   // console.log(window.event);
+//   console.log(value);
+// }
+
+// function itsHovered(){
+//   console.log("Box is Hovered");
+// }
+
+// function itsOut(){
+//   console.log("Box is not Hovered");
+// }
+
+// function itsMoving(){
+//   console.log(window.event);
 // }
 
 
-// function printSum() {
-//   sum();
+// window.onload = function(){
+
+//   let testelement = document.getElementById('test');
+//   testelement.textContent = 'salam';
+//   console.log(testelement);
+
+// }
+
+// function runjs(){
+
+//     let testelement = document.getElementById('test');
+//     testelement.textContent = 'salam';
+//     console.log(testelement);
+
 // }
 
 
+/* trigger example for later ************ */
+// function slide(){
+//   let activeSlider = document.getElementById('activeSlider')
+//   $(activeSlider).css({
+//     'opacity': 0,
+//     'display': 'none'
+//   })
+// }
 
-// sumNum = sum(4, 5);
-// c = multiple(2, sumNum);
-// console.log(c);
+// setInterval(function(){
+
+//   let nexArrow = document.getElementById('activeSlider')
+//   nexArrow.trigger('click')
+
+// }, 6000)
+/************* */
 
 
-// let car = ['aa', 'bb'];
-// console.log(car);
+
+// document.getElementById('click-here').addEventListener('click', function(){
+//   console.log('farzin');
+// })
+
+// let targetElement = document.getElementById('div');
+// targetElement.addEventListener('mousemove', function(e){
+//   console.log(e);
+//   targetElement.textContent = `X: ${e.clientX} Y:${e.clientY}`;
+// })
 
 
-/* Objects ******************** */
-// let person1;
-
-// person1 = {
-//   firstName: "Farzin",
-//   lastName: "Ahamadi",
-//   age: 26,
-//   hobby: [
-//     'Video Game',
-//     'Movie',
-//     'Anime'
-//   ],
-//   printFullName: function(){
-//     console.log(`${this.firstName} ${this.lastName}`);
-//   }
+/* Conditions************** */
+// let person1 = {
+//   firstName: 'farzin',
+//   lastName: 'ahmady',
+//   age: 25.5
 // };
 
-// person1.nationalCode = '0017306140';
-
-// console.log(person1);
-// console.log(person1.firstName);
-// console.log(person1.hobby[1]);
-// console.log(person1.age);
-
-// console.log(typeof person1);
-// console.log(typeof person1.hobby[1]);
-// console.log(typeof person1.age);
+// if(person1.age < 40 && person1.age >= 12 && person1.firstName == 'Amir') {
+//   console.log('young');
+// }else if(person1.age<20) {
+//   console.log('kid');
+// }else{
+//   console.log('middle age');
+// }
 
 
-/* Events ****************** */
-function printFirstName() {
-  console.log("Farzin");
+// switch (person1.age){
+//   case 26:
+//   case 25:
+//     console.log('young');
+//     break;
+  
+//   case 12:
+//     console.log('kid');
+//     break;
+  
+//   case 12:
+//     console.log('kid');
+//     break;
+
+//   default:
+//     console.log('i don\'t know')
+// }
+/*************** */
+
+
+
+
+/* Loop*********** */
+let cars = ['bmw m5', 'nissan gtr', 'mercedes slr'];
+// cars[10] = 'test car';
+
+console.log(cars);
+
+
+// for (let i = 0; i < cars.length; i++) {
+//   console.log(cars[i]);
+// }
+
+// for (let i in cars) {
+//   console.log(cars[i]);
+// }
+
+// let i = 0;
+// while(i < cars.length){
+
+//   console.log(cars[i]);
+//   i++;
+// }
+
+// let i = 25;
+// do{
+//   console.log(cars[i]);
+
+//   i++;
+// }while(i < cars.length);
+
+// cars.forEach(function(car){
+//   console.log(car);
+// });
+
+
+let i = 0;
+while( true ){
+  i++;
+
+  if(i>4 && i<=100){
+    continue;
+  }
+
+
+  console.log(i);
+  if(i >= 10){
+    break;
+  }
+
 }
-
-function printVal(value){
-  // console.log(window.event);
-  console.log(value);
-}
-
-function itsHovered(){
-  // console.log(window.event);
-  console.log("Box is Hovered");
-}
-
-function itsOut(){
-  console.log("Box is not Hovered");
-}
-
-let a, b;
-a = 5;
-b = 4;
-
-// setTimeout(function() {
-//   console.log(a + b);
-// }, 2000);
-
-// setInterval(function() {
-//   console.log(a);
-//   a += 1;
-// }, 2000);
