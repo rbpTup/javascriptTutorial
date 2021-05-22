5ac6c7582fe9e01b3f9a29808529446f430ba794
enigma01Z313 <f.ahmadyf94@gmail.com>
2021-05-22 18:35:29 +0430

    exercise7

--- js/main.js
+++ js/main.js
@@ -1,66 +1,40 @@
-const targetElement = document.getElementById('test');
-// console.log(targetElement);
-let testTxt = 'Hi';
+const closestSide = sides => {
+  let minVal = 1000000, minIndex;
 
-// targetElement.textContent = "<h2>Hi</h2>";
-// targetElement.innerHTML = "<h2>Hi</h2>";
-// targetElement.innerHTML = `<h2>${testTxt}</h2>`;
+  for (let i in sides) {
+    if( sides[i] < minVal ){
+      minVal = sides[i];
+      minIndex = i;
+    }
+  }
 
-
-
-// const paraph = document.createElement('p');
-// const articleTag = document.createElement('div');
-// const someTxt = document.createTextNode(' test');
-// const someTxt2 = document.createTextNode(' test');
-// paraph.textContent = 'some text';
-// paraph.appendChild(someTxt);
-// paraph.appendChild(someTxt2);
-// articleTag.classList.add('class1');
-// articleTag.id = 'box2';
-// articleTag.textContent = 'box2';
-// articleTag.appendChild(paraph);
-
-// console.log(typeof articleTag);
-// console.log(articleTag);
-
-
-// targetElement.appendChild(articleTag);
-// targetElement.prepend(articleTag);
-
-// const heading = document.createElement('h2');
-// heading.textContent = 'Header';
-// const myPraph = document.getElementById('firstParaph');
-
-
-// targetElement.insertBefore(heading, myPraph);
-// targetElement.after(heading);
-// targetElement.before(heading);
-
-
-// const mySecondParaph = targetElement.getElementsByTagName('p')[1];
-// mySecondParaph.remove();
-// console.log(mySecondParaph);
-
-// const targetParaph = targetElement.getElementsByTagName('p')[1];
-// const targetParaph2 = targetElement.getElementsByTagName('p')[2];
-// targetElement.removeChild(targetParaph);
-// targetElement.removeChild(targetParaph2);
-
-// const newPraph = document.createElement('p');
-// newPraph.textContent = 'new paragraph';
-// const targetParaph = targetElement.getElementsByTagName('p')[1];
-// targetElement.replaceChild(newPraph, targetParaph);
-// targetElement.replaceChild(targetParaph, newPraph);
-
-const txt = "salam";
-const newElement = `<h2>${txt}</h2>`;
-myPrepend(targetElement, newElement);
-
-
-
-function myPrepend(target, el) {
-  target.innerHTML = `${el} ${target.innerHTML}`;
+  return minIndex;
 }
-function myAppend(target, el) {
-  target.innerHTML = `${target.innerHTML} ${el}`;
+
+const mouseEntered = (e) => {
+  let sides = {}, text;
+
+  if(e.type === 'mouseover') {
+    sides.top = e.layerY;
+    sides.right = e.target.clientWidth - e.layerX;
+    sides.bottom = e.target.clientHeight - e.layerY;
+    sides.left = e.layerX;
+
+    text = `Entrance from: ${closestSide(sides)}`;
+  }else if(e.type === 'mouseout'){
+    console.log(e);
+    sides.top = Math.abs(e.clientY - e.target.offsetTop);
+    sides.right = Math.abs(e.clientX - e.target.clientWidth - e.target.offsetLeft);
+    sides.bottom = Math.abs(e.clientY - e.target.clientHeight - e.target.offsetTop);
+    sides.left = Math.abs(e.clientX - e.target.offsetLeft);
+
+    text = `Exit from: ${closestSide(sides)}`;
+  }
+
+  e.target.textContent = text
+}
+
+function menuTrigger(e) {
+  e.target.classList.toggle('fa-bars');
+  e.target.classList.toggle('fa-times');
 }
